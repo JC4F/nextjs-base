317ef85b094422a6ac4747daba8bfec0c49e998d
lequan12 <lequan12@tebprint.com>
2024-01-05 12:02:26 +0700

    feat: add google auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,43 +1,56 @@
 import type { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const options: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+    // signOut: "/singOut",
+  },
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: {
-          label: 'Username:',
-          type: 'text',
-          placeholder: 'your-cool-username',
-        },
-        password: {
-          label: 'Password:',
-          type: 'password',
-          placeholder: 'your-awesome-password',
-        },
-      },
-      async authorize(credentials) {
-        // This is where you need to retrieve user data
-        // to verify with credentials
-        // Docs: https://next-auth.js.org/configuration/providers/credentials
-        const user = { id: '42', name: 'peter', password: '123456' };
-
-        if (credentials?.username === user.name && credentials?.password === user.password) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
   ],
-  pages: {
-    signIn: '/sign-in',
-    signOut: '/sign-out',
+  callbacks: {
+    // async session({ token, session }) {
+    //   if (token) {
+    //     session.user.id = token.id
+    //     session.user.name = token.name
+    //     session.user.email = token.email
+    //     session.user.image = token.picture
+    //   }
+
+    //   return session
+    // },
+    // async jwt({ token, user }) {
+    //   const dbUser = await db.user.findFirst({
+    //     where: {
+    //       email: token.email,
+    //     },
+    //   })
+
+    //   if (!dbUser) {
+    //     if (user) {
+    //       token.id = user?.id
+    //     }
+    //     return token
+    //   }
+
+    //   return {
+    //     id: dbUser.id,
+    //     name: dbUser.name,
+    //     email: dbUser.email,
+    //     picture: dbUser.image,
+    //   }
+    // },
   },
 };
