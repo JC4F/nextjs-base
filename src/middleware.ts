b975b22ed2f40cf925e586444620c789f53fe2b0
lequan12 <lequan12@tebprint.com>
2024-01-14 22:42:30 +0700

    fix: build intl error

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,4 +20,13 @@ export default withAuth(
 // https://github.com/vercel/next.js/discussions/36308
 export const config = {
   matcher: '/((?!api|static|.*\\..*|_next).*)',
+  // fix build dynamic error
+  unstable_allowDynamic: [
+    '/node_modules/.pnpm/@babel+runtime@7.23.6/node_modules/@babel/runtime/regenerator/index.js',
+    '/node_modules/.pnpm/next-auth@4.24.5_next@14.0.4_react-dom@18.2.0_react@18.2.0/node_modules/next-auth/react/index.js',
+    '/src/lib/custom-fetch.ts',
+    '/src/lib/index.ts',
+    '/src/middlewares/intl.ts',
+    '/src/middlewares/index.ts', // use a glob to allow anything in the function-bind 3rd party module
+  ],
 };
